a3e5360077cc964035a8e6b153f3e776d52bce29
Justin Lieberthal <justin@gqpbj.com>
2013-01-17 17:33:18 +0800

    Test spider

---
+++ tutorial/pipelines.py
@@ -0,0 +1,56 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: http://doc.scrapy.org/topics/item-pipeline.html
+
+#class TutorialPipeline(object):
+    #def process_item(self, item, spider):
+        #return item
+
+import pymongo
+
+from scrapy.exceptions import DropItem
+from scrapy.conf import settings
+from scrapy import log
+from scrapy import signals
+
+
+class DuplicatesPipeline(object):
+
+    def __init__(self):
+        self.titles_seen = set()
+
+    def process_item(self, item, spider):
+        if item['title'] in self.titles_seen:
+            raise DropItem("Duplicate item found: %s" % item)
+        else:
+            self.titles_seen.add(item['title'])
+            return item
+
+class NoData(object):
+    def process_item(self, item, spider):
+        if len(item['title']) > 0:
+            return item
+        else:
+            raise DropItem("Missing title in %s" % item)
+
+class MongoDBPipeline(object):
+    def __init__(self):
+        connection = pymongo.Connection(settings['MONGODB_SERVER'], settings['MONGODB_PORT'])
+        db = connection[settings['MONGODB_DB']]
+        self.collection = db[settings['MONGODB_COLLECTION']]
+        
+    def process_item(self, item, spider):
+    	valid = True
+        for data in item:
+          # here we only check if the data is not null
+          # but we could do any crazy validation we want
+       	  if not data:
+            valid = False
+            raise DropItem("Missing %s of blogpost from %s" %(data, item['url']))
+        if valid:
+          self.collection.insert(dict(item))
+          log.msg("Item wrote to MongoDB database %s/%s" %
+                  (settings['MONGODB_DB'], settings['MONGODB_COLLECTION']),
+                  level=log.DEBUG, spider=spider) 
+        return item
---
+++ tutorial/spiders/bass_spider.py
@@ -0,0 +1,36 @@
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.selector import HtmlXPathSelector
+from tutorial.items import BassItem
+
+import logging
+from scrapy.log import ScrapyFileLogObserver
+
+logfile = open('testlog.log', 'w')
+log_observer = ScrapyFileLogObserver(logfile, level=logging.DEBUG)
+log_observer.start()
+
+
+class BassSpider(CrawlSpider):
+    name = "bass"
+    allowed_domains = ["talkbass.com"]
+    start_urls = ["http://www.talkbass.com/forum/f126"]
+
+
+    rules = [Rule(SgmlLinkExtractor(allow=['/f126/index*']), callback='parse_item', follow=True)]
+
+
+    def parse_item(self, response):
+
+        hxs = HtmlXPathSelector(response)
+
+
+        ads = hxs.select('//table[@id="threadslist"]/tbody/tr/td[@class="alt1"][2]/div')
+        items = []
+        for ad in ads:
+            item = BassItem()
+            item['title'] = ad.select('a/text()').extract()
+            item['link'] = ad.select('a/@href').extract()
+            items.append(item)
+        return items
+
